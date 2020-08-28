097ee91a444648b6f06b3bb0f082428a24785213
Tommy Hudson <thomhuds@protonmail.com>
2020-08-28 21:34:47 +0100

    Fix NPE on player interaction with air

--- src/main/java/com/github/thomhuds/unobsidian/ObsidianListener.java
+++ src/main/java/com/github/thomhuds/unobsidian/ObsidianListener.java
@@ -2,6 +2,7 @@ package com.github.thomhuds.unobsidian;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -10,8 +11,12 @@ import org.bukkit.inventory.ItemStack;
 public class ObsidianListener implements Listener {
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
+        Block block = event.getClickedBlock();
+        if (block == null) {
+            return;
+        }
         if (event.getMaterial().equals(Material.BUCKET)
-                && event.getClickedBlock().getType().equals(Material.OBSIDIAN)) {
+                && block.getType().equals(Material.OBSIDIAN)) {
             ItemStack item = event.getItem();
             item.setAmount(item.getAmount() - 1);
             event.getClickedBlock().setType(Material.AIR, true);
